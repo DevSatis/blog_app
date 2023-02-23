d424bb8315b406edcf234f5ccc8707b896366c21
DevSatis <sksatishk9@gmail.com>
2023-02-23 23:37:12 +0530

    Connecting to backend with FrontEnd

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,21 @@
 import "./Home.css";
 
-import React from "react";
+import React, { useEffect, useState  } from "react";
 import Header from "../../components/header/Header";
 import Posts from "../../components/posts/Posts";
 import Sidebar from "../../components/sidebar/Sidebar";
+import axios from "axios";
 
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(()=>{
+    const fetchPost = async ()=>{
+      const res = await axios.get("http://localhost:5000/api/posts");
+      console.log(res);
+    }
+    fetchPost();
+  },[])
   return (
     <>
       <Header />

4c08814c03137e52ddc4d8b9a1c4b680e3c7891c
DevSatis <sksatishk9@gmail.com>
2023-03-14 12:10:52 +0530

    Small_change

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -1,50 +1,65 @@
 import "./SinglePost.css";
 import BorderColorOutlinedIcon from "@mui/icons-material/BorderColorOutlined";
 import DeleteIcon from "@mui/icons-material/Delete";
-import React, { useEffect, useState } from "react";
-import {useLocation} from "react-router";
+import React, { useEffect, useState, useContext } from "react";
+import { useLocation } from "react-router";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import { Context } from "../../context/Context";
 
 const SinglePost = () => {
 
   const location = useLocation();
   const path = location.pathname.split("/")[2];
   const [post, setPost] = useState({});
+  const { user } = useContext(Context);
 
   const PF = "http://localhost:5000/images/";
 
-  useEffect(()=>{
-    const getPost = async()=>{
+  useEffect(() => {
+    const getPost = async () => {
       const res = await axios.get("/api/posts/" + path);
       setPost(res.data);
     }
     getPost();
-  },[path])
+  }, [path]);
+
+  const handleDelete =async()=>{
+    try{
+      await axios.delete(`/api/posts/${post._id}`, {data:{username: user.username}});
+      window.location.replace("/");
+    }catch(err){
+
+    }
+  }
 
   return (
     <div className="singlePost">
       <div className="singlePostWrapper">
         {post.photo && (
           <img
-          src={PF + post.photo}
-          alt=""
-          className="singlePostImg"
-        />
+            src={PF + post.photo}
+            alt=""
+            className="singlePostImg"
+          />
         )}
-        
+
         <h1 className="singlePostTitle">
           {post.title}
-          <div className="singlePostEdit">
-            <BorderColorOutlinedIcon className="singlePostIcon" />
-            <DeleteIcon className="singlePostIcon" />
-          </div>
+          {post.username === user.username &&
+            (
+              <div className="singlePostEdit">
+                <BorderColorOutlinedIcon className="singlePostIcon" />
+                <DeleteIcon className="singlePostIcon" onClick={handleDelete} />
+              </div>
+            )}
+
         </h1>
         <div className="singlePostInfo">
           <span className="singlePostAuthor">
-            Author: 
+            Author:
             <Link to={`/?user=${post.username}`} className="link"> <b>{post.username}</b> </Link>
-           
+
           </span>
           <span className="singlePostDate">{new Date(post.createdAt).toDateString()}</span>
         </div>
